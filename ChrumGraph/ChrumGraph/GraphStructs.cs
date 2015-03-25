3f17a190c3770454c9e5977d994fcf5ee94e8b67
Marcin Papierzyński <marcin.papierzynski@student.uw.edu.pl>
2015-03-25 19:22:23 +0100

    Główny projekt
    Stworzony projekt WPF + drobne zmiany w interfejsie

---
+++ ChrumGraph/ChrumGraph/GraphStructs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrumGraph
+{
+	public partial class Vertex
+	{
+		public double X, Y; //położenie w Core
+		public bool Pinned;
+
+		//położenie wymuszone przez użytkownika
+		public bool PositionForced;
+		public double ForcedX, ForcedY;
+	}
+
+	public partial class Edge
+	{
+		public Vertex V1, V2;
+	}
+}
