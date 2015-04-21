1316f527582145ffe8d655c962742e46c146315e
Filip Kowalski <filipko133@gmail.com>
2015-04-21 11:52:21 +0200

    Created main window and canvas

--- ChrumGraph/ChrumGraph/Classes/Visual.cs
+++ ChrumGraph/ChrumGraph/Classes/Visual.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ChrumGraph
 {
-    class Visual
+    class Visual : IVisual
     {
+        public Visual()
+        {
+  
+        }
+        public void CreateVisualVertex(Vertex v)
+        { }
+        public void RemoveVisualVertex(Vertex v)
+        { }
+        public void CreateVisualEdge(Edge e)
+        { }
+        public void RemoveVisualEdge(Edge e)
+        { }
+        public bool Visible
+        {
+            set
+            {
+
+            }
+            get
+            {
+                return true;
+            }
+        }
+        public void Refresh()
+        { }
     }
 }
--- ChrumGraph/ChrumGraph/MainWindow.xaml.cs
+++ ChrumGraph/ChrumGraph/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,14 +23,54 @@ namespace ChrumGraph
     public partial class MainWindow : Window
     {
         Core core;
-        Visual visual;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            //visual = new Visual(...);
-            //core = new Core(visual);
+            core = new Core();
+        }
+
+        private void bOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openDialog = new Microsoft.Win32.OpenFileDialog();
+            openDialog.DefaultExt = ".graph";
+            openDialog.Title = "Open graph from a file";
+            Nullable<bool> result = openDialog.ShowDialog();
+            List<Vertex> vertices = core.Vertices;
+            List<Edge> edges = core.Edges;
+            if (result == true)
+            {
+                string filePath = openDialog.FileName;
+                StreamReader reader = new StreamReader(filePath);
+                string input = reader.ReadToEnd();
+                reader.Close();
+
+            }
+        }
+
+        private void bSave_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog saveDialog = new Microsoft.Win32.SaveFileDialog();
+            saveDialog.DefaultExt = ".graph";
+            saveDialog.Title = "Save graph to a file";
+            saveDialog.ShowDialog();
+            List<Vertex> vertices = core.Vertices;
+            List<Edge> edges = core.Edges;
+
+            if (saveDialog.FileName != "")
+            {
+                FileStream fs = (FileStream)saveDialog.OpenFile();
+                UnicodeEncoding uniEncoding = new UnicodeEncoding();
+                string line = vertices.Count.ToString() + " " + edges.Count.ToString() + Environment.NewLine;
+                fs.Write(uniEncoding.GetBytes(line), 0, uniEncoding.GetByteCount(line));
+                foreach (Edge edge in edges)
+                {
+                    line = edge.V1.ToString() + " " + edge.V2.ToString() + Environment.NewLine;
+                    fs.Write(uniEncoding.GetBytes(line), 0, uniEncoding.GetByteCount(line));
+                }
+                fs.Close();
+            }
         }
     }
 }
