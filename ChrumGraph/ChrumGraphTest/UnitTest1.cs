29564efb06da5cbfdfdb0e82739b8cd6ce92d8d8
sneapewa <sneape@tlen.pl>
2015-04-25 13:45:00 +0200

    Tests: test for loading graph from file

--- ChrumGraph/ChrumGraphTest/UnitTest1.cs
+++ ChrumGraph/ChrumGraphTest/UnitTest1.cs
@@ -17,6 +17,7 @@ namespace ChrumGraphTest
             Edge e1 = new Edge(v1, v2);
         }
     }
+
     [TestClass]
     public class PhysicsTests
     {
@@ -28,6 +29,41 @@ namespace ChrumGraphTest
             double v = p.VertexForceParam;
             double e = p.EdgeForceParam;
             double f = p.FrictionParam;
+        }
+    }
+
+    [TestClass]
+    public class LoadSaveTest
+    {
+        private void WriteOnConsole(Core c)
+        {
+            Console.WriteLine("Vertices:");
+            foreach (Vertex v in c.Vertices)
+            {
+                Console.WriteLine("label = " + v.Label);
+                string tab = "   ";
+                Console.WriteLine(tab + "x = " + Math.Round(v.X, 2));
+                Console.WriteLine(tab + "y = " + Math.Round(v.Y, 2));
+            }
+            Console.WriteLine("Edges:");
+            foreach (Edge e in c.Edges)
+            {
+                Console.Write(e.V1.Label);
+                Console.Write(", ");
+                Console.Write(e.V2.Label);
+                Console.WriteLine();
+            }
+        }
+
+        [TestMethod]
+        public void Load()
+        {
+            Core c = new Core();
+            //Below you should use your own path to your own load.txt file
+            //string file1 = @"LoadSaveFiles\load.txt";
+            //c.LoadFromFile(file1);
+
+            WriteOnConsole(c);
         }
     }
 }
